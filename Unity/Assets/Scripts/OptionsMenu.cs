74b42f2059a3ea02b0d194b220f3438b1dd3f30e
aron64 <aronhertendi@gmail.com>
2019-11-02 01:23:00 +0100

    Beállítások menü backend

--- Unity/Assets/Scripts/OptionsMenu.cs
+++ Unity/Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Beállítások menü:
+/// Hang és Pályaméret beállítása
+/// </summary>
 public class OptionsMenu : MonoBehaviour
 {
+    /// <summary>
+    /// Referencia a csúszkához
+    /// </summary>
+    public UnityEngine.UI.Slider MapSlider;
+    /// <summary>
+    /// Referencia a Pályaméret megjelenítéséhez
+    /// </summary>
+    public UnityEngine.UI.Text SizeText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MapSlider.minValue = 8;
+        MapSlider.maxValue = 20;
+        MapSlider.value = 8;
+        MapSlider.wholeNumbers = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    /// <summary>
+    /// A pálya méretének frissítése globálisan
+    /// </summary>
+    public void RefreshValue()
+    {
+        SizeText.text = MapSlider.value.ToString();
+        SizeText.text += "x" + SizeText.text;
+        Map.MapSize = (int)MapSlider.value;
     }
 }
